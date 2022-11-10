f12ff7ecd016d2f09b5d19f19e91129d09761036
Daniel Ukaji <dannyukaji@gmail.com>
2022-11-10 13:32:44 +0100

    Progress Bar

--- pages/_app.js
+++ pages/_app.js
@@ -14,9 +14,19 @@ import { GlobalProvider } from '../context/GlobalState'
 import ProgressBar from "@badrap/bar-of-progress";
 import Router from "next/router";
 
-function MyApp({ Component, pageProps }) {
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+})
 
-  
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
+
+function MyApp({ Component, pageProps }) {
 
   const [queryClient] = useState(() => new QueryClient());
 
@@ -43,17 +53,7 @@ function MyApp({ Component, pageProps }) {
     restoreUser();
   }, []);
 
-  const progress = new ProgressBar({
-    size: 4,
-    color: "#FE595E",
-    className: "z-50",
-    delay: 100,
-  })
-
-
-  Router.events.on('routeChangeStart', progress.start)
-  Router.events.on('routeChangeComplete', progress.finish)
-  Router.events.on('routeChangeError', progress.finish)
+  
 
   return (
   <GlobalProvider>
